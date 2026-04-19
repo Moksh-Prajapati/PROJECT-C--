f730136197c412665072a729def94ff1f62c0ad2
Arshhh26 <reshmaanis1979@gmail.com>
2026-04-19 17:49:02 +0530

    hello world

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+class user {
+private:
+    string username;
+    string password;
+    string roomtype;
+public:
+    user(string a, string b, stringc= "") {
+        username = a;
+        password = b;
+        roomtype = c; 
+    }
+    string getusername(){
+        return username;
+    }
+    string getpassword(){
+        return password;
+    }   
+    string getroomtype(){
+        return roomtype;
+    }   
+};
+
+
+
+class manager{
+    public:
+
+    void userregistration()
+    { string username , string password , string roomtype;
+        cout<< "\n Registeration Section\n";  \
+        cout<<"Enter username:";
+        cin>>username;
+
+        cout<<"Enter password:";
+        cin>>password;
+        
+        ofstream file("login.txt",ios::app);
+        file<<username<<" "<<password<<" "<<endl;
+        file.close();
+
+        cout<<"Registration successful!"<<endl;
+
+    } 
+
+
+
+
+}
